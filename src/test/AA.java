90835c6dacc994bf642a2c9a05f59adba5db55a6
hellobpm <hellobpm@gmail.com>
2019-01-30 14:09:53 +0900

    Change-Id: I0000000000000000000000000000000000000000 Signed-off-by: hellobpm <hellobpm@gmail.com>

--- src/test/AA.java
+++ src/test/AA.java
@@ -5,8 +5,12 @@ import java.util.stream.Stream;
 public class AA {
 
 	public static void main(String[] args) {
+		
+		args = new String[] {"aa", "bb", "cc"};
+		
 		System.out.println("hello");
 		System.out.println(args.length);
 		Stream.of(args).forEach(System.out::println);
+		
 	}
 }
